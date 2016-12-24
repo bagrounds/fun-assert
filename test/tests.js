fb60715a45f5d76a585560debb6b1e9f64f1360e
bagrounds <bagrounds@gmail.com>
2016-12-24 00:18:42 -0800

    [MINOR][ADD] return original value from all assertions

--- test/tests.js
+++ test/tests.js
@@ -8,7 +8,15 @@
 
   /* exports */
   module.exports = [
-    test1()
+    test1(),
+    test2(),
+    test3(),
+    test4(),
+    test5(),
+    test6(),
+    test7(),
+    test8(),
+    test9()
   ]
 
   function test1 () {
@@ -25,11 +33,227 @@
     test.description = 'Should export full api'
 
     return test
+
+    function transformer (funAssert) {
+      return function () {
+        return apiSpecChecker(funAssert)
+      }
+    }
   }
 
-  function transformer (funAssert) {
-    return function () {
-      return apiSpecChecker(funAssert)
+  function test2 () {
+    var testInput = 'test input'
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.type('String')
+    }
+  }
+
+  function test3 () {
+    var testInput = 42
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.equal(testInput)
+    }
+  }
+
+  function test4 () {
+    var testInput = 'something 3242 to match'
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.match(/^\S+ \d+.*$/)
+    }
+  }
+
+  function test5 () {
+    var testInput = null
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.falsey
+    }
+  }
+
+  function test6 () {
+    var testInput = 1
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.truthy
+    }
+  }
+
+  function test7 () {
+    var testInput = 'string'
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.or([funAssert.falsey, funAssert.type('String')])
+    }
+  }
+
+  function test8 () {
+    var testInput = 99
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.and([funAssert.truthy, funAssert.type('Number')])
+    }
+  }
+
+  function test9 () {
+    var testInput = 99
+
+    var test = funTest({
+      input: testInput,
+      verifier: function verifier (error, result) {
+        if (error) {
+          throw new VError(error, 'Should not throw error')
+        }
+
+        if (result !== testInput) {
+          throw new Error('Should return input')
+        }
+      },
+      transformer: transformer,
+      sync: true
+    })
+
+    test.description = 'Assertions should return original values'
+
+    return test
+
+    function transformer (funAssert) {
+      return funAssert.not(funAssert.falsey)
     }
   }
 })()
