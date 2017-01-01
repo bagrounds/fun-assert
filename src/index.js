8fce05a3ed7052cc2ed07940946a5d12c852af8e
bagrounds <bagrounds@gmail.com>
2017-01-01 03:38:41 -0800

    [MINOR][CHANGE] rewrite module using fun-predicate and tests using latest fun-test

--- src/index.js
+++ src/index.js
@@ -2,271 +2,42 @@
   'use strict'
 
   /* imports */
-  var typeCheck = require('type-check').typeCheck
-  var stringifySafe = require('json-stringify-safe')
-  var deepEqual = require('deep-equal')
+  var funPredicate = require('fun-predicate')
+  var stringify = require('stringify-anything')
 
-  /* exports */
-  module.exports = {
-    truthy: truthy(),
-    falsey: falsey(),
-    type: type,
-    equal: equal,
-    match: match,
-    or: or,
-    and: and,
-    not: not
-  }
-
-  function truthy () {
-    function asserter (subject) {
-      if (!subject) {
-        throw error(subject)
-      }
-
-      return subject
-    }
-
-    function error (subject) {
-      var verb = 'be'
-
-      return makeError(subject, verb, 'truthy')
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'isTruthy()'
-    }
-
-    return asserter
-  }
-
-  function falsey () {
-    function asserter (subject) {
-      if (subject) {
-        throw error(subject)
-      }
-
-      return subject
-    }
-
-    function error (subject) {
-      var verb = 'be'
-
-      return makeError(subject, verb, 'falsey')
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'isFalsey()'
-    }
-
-    return asserter
-  }
+  module.exports = funAssert
+  module.exports.truthy = funAssert(funPredicate.truthy)
+  module.exports.falsey = funAssert(funPredicate.falsey)
+  module.exports.equal = equal
+  module.exports.type = type
+  module.exports.match = match
 
   function equal (reference) {
-    function asserter (subject) {
-      var ok = reference === subject
-
-      if (typeCheck('Object|Array', reference)) {
-        ok = deepEqual(subject, reference, {strict: true})
-      }
-
-      if (!ok) {
-        throw error(subject)
-      }
-
-      return subject
-    }
-
-    function error (subject) {
-      var verb = 'equal'
-
-      return makeError(subject, verb, reference)
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'equal(' + reference + ')'
-    }
-
-    return asserter
-  }
-
-  function match (reference) {
-    function asserter (subject) {
-      var ok = reference.test(subject)
-
-      if (!ok) {
-        throw error(subject)
-      }
-
-      return subject
-    }
-
-    function error (subject) {
-      var verb = 'match'
-
-      return makeError(subject, verb, reference.source)
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'match(' + reference + ')'
-    }
-
-    return asserter
+    return funAssert(funPredicate.equal(reference))
   }
 
   function type (reference) {
-    function asserter (subject) {
-      var ok = typeCheck(reference, subject)
+    return funAssert(funPredicate.type(reference))
+  }
 
-      if (!ok) {
-        throw error(subject)
+  function match (reference) {
+    return funAssert(funPredicate.match(reference))
+  }
+
+  function funAssert (predicate) {
+    function result (subject) {
+      if (!predicate(subject)) {
+        throw new Error(stringify(predicate))
       }
 
       return subject
     }
 
-    function error (subject) {
-      var verb = 'have type'
-
-      return makeError(subject, verb, reference)
+    result.toString = function toString () {
+      return stringify(predicate)
     }
 
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'hasType(' + reference + ')'
-    }
-
-    return asserter
-  }
-
-  function and (asserters) {
-    function asserter (subject) {
-      asserters.forEach(function (asserter) {
-        try {
-          asserter(subject)
-        } catch (e) {
-          throw error(subject)
-        }
-      })
-
-      return subject
-    }
-
-    function error (subject) {
-      var errors = asserters.map(function (asserter) {
-        var error = asserter.error(subject)
-
-        error.message = '(' + error.message + ')'
-
-        return error
-      })
-
-      var combinedError = errors.reduce(function (combined, error) {
-        combined.message += ' AND ' + error.message
-
-        return combined
-      })
-
-      return combinedError
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'AND(' + asserters + ')'
-    }
-
-    return asserter
-  }
-
-  function or (asserters) {
-    function asserter (subject) {
-      var errors = asserters.reduce(function (results, asserter) {
-        try {
-          asserter(subject)
-        } catch (error) {
-          results.push(error)
-        }
-
-        return results
-      }, [])
-
-      if (errors.length === asserters.length) {
-        throw error(subject)
-      }
-
-      return subject
-    }
-
-    function error (subject) {
-      var errors = asserters.map(function (asserter) {
-        var error = asserter.error(subject)
-
-        error.message = '(' + error.message + ')'
-
-        return error
-      })
-
-      var combinedError = errors.reduce(function (combined, error) {
-        combined.message += ' OR ' + error.message
-
-        return combined
-      })
-
-      return combinedError
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'OR(' + asserters + ')'
-    }
-
-    return asserter
-  }
-
-  function not (originalAsserter) {
-    function asserter (subject) {
-      try {
-        originalAsserter(subject)
-      } catch (error) {
-        return subject
-      }
-
-      throw error(subject)
-    }
-
-    function error (subject) {
-      var original = originalAsserter.error(subject)
-
-      original.message = 'NOT(' + original.message + ')'
-      return original
-    }
-
-    asserter.error = error
-
-    asserter.toString = function () {
-      return 'NOT(' + originalAsserter + ')'
-    }
-
-    return asserter
-  }
-
-  function makeError (subject, verb, reference) {
-    var s = stringifySafe(subject)
-    var r = stringifySafe(reference)
-    var message = s + ' should ' + verb + ' ' + r
-
-    return new Error(message)
+    return result
   }
 })()
 
--- test/tests.js
+++ test/tests.js
@@ -3,243 +3,143 @@
 
   /* imports */
   var funTest = require('fun-test')
-  var VError = require('verror')
-  var apiSpecChecker = require('../src/spec-api')
-  var globalFunAssert = require('../src/')
-  var stringifier = require('stringifier')
+  var funAssert = require('../src/')
+  var funTransform = require('fun-transform')
+  var funPredicate = require('fun-predicate')
 
   /* exports */
   module.exports = [
-    testApiSpec()
-  ]
-
-  function testApiSpec () {
-    var test = funTest({
-      verifier: function verifier (error) {
-        if (error) {
-          throw new VError(error, 'Should not throw error')
-        }
-      },
-      transformer: transformer,
-      sync: true
-    })
-
-    test.description = 'Should export full api'
-
-    return test
-
-    function transformer (funAssert) {
-      return function () {
-        return apiSpecChecker(funAssert)
-      }
+    {
+      input: ' ',
+      sync: true,
+      transformer: funTransform.toMethod('truthy'),
+      result: funAssert.equal(' ')
+    },
+    {
+      input: '',
+      sync: true,
+      transformer: funTransform.toMethod('truthy'),
+      error: funAssert.truthy
+    },
+    {
+      input: ' ',
+      sync: true,
+      transformer: funTransform.toMethod('falsey'),
+      error: funAssert.truthy
+    },
+    {
+      input: '',
+      sync: true,
+      transformer: funTransform.toMethod('falsey'),
+      result: funAssert.equal('')
+    },
+    {
+      input: ' ',
+      sync: true,
+      transformer: funTransform.toMethod('equal'),
+      result: funAssert.type('Function')
+    },
+    {
+      input: 5,
+      sync: true,
+      transformer: equal5,
+      result: funAssert.equal(5)
+    },
+    {
+      input: 4,
+      sync: true,
+      transformer: equal5,
+      error: funAssert.truthy
+    },
+    {
+      input: /\d/,
+      sync: true,
+      transformer: funTransform.toMethod('match'),
+      result: funAssert.type('Function')
+    },
+    {
+      input: 'a digit: 5!',
+      sync: true,
+      transformer: matchDigit,
+      result: funAssert.equal('a digit: 5!')
+    },
+    {
+      input: 'no digit!',
+      sync: true,
+      transformer: matchDigit,
+      error: funAssert.truthy
+    },
+    {
+      input: funPredicate.or(
+        funPredicate.type('Number'),
+        funPredicate.type('String')
+      ),
+      sync: true,
+      result: funAssert.type('Function')
+    },
+    {
+      input: 5,
+      transformer: funTransform.toResult(
+        funPredicate.or(
+          funPredicate.type('Number'),
+          funPredicate.type('String')
+        )
+      ),
+      sync: true,
+      result: funAssert.equal(5)
+    },
+    {
+      input: 'a string',
+      transformer: funTransform.toResult(
+        funPredicate.or(
+          funPredicate.type('Number'),
+          funPredicate.type('String')
+        )
+      ),
+      sync: true,
+      result: funAssert.equal('a string')
+    },
+    {
+      input: [],
+      transformer: funTransform.toResult(
+        funPredicate.or(
+          funPredicate.type('Number'),
+          funPredicate.type('String')
+        )
+      ),
+      sync: true,
+      error: funAssert.truthy
+    },
+    {
+      input: {},
+      transformer: funTransform.toResult(
+        funPredicate.or(
+          funPredicate.type('Number'),
+          funPredicate.type('String')
+        )
+      ),
+      sync: true,
+      error: funAssert.truthy
+    },
+    {
+      input: null,
+      transformer: funTransform.toResult(
+        funPredicate.or(
+          funPredicate.type('Number'),
+          funPredicate.type('String')
+        )
+      ),
+      sync: true,
+      error: funAssert.truthy
     }
+  ].map(funTest)
+
+  function equal5 (funAssert) {
+    return funAssert.equal(5)
   }
 
-  module.exports = module.exports.concat(
-    [
-      {
-        method: 'falsey',
-        testInput: '',
-        shouldPass: true
-      },
-      {
-        method: 'falsey',
-        testInput: ' ',
-        shouldPass: false
-      },
-      {
-        method: 'truthy',
-        testInput: ' ',
-        shouldPass: true
-      },
-      {
-        method: 'truthy',
-        testInput: '',
-        shouldPass: false
-      }
-    ].map(testAssertion)
-  )
-
-  function testAssertion (options) {
-    var test = funTest({
-      input: options.testInput,
-      verifier: verifier,
-      transformer: transformToMethod(options.method),
-      sync: true
-    })
-
-    function verifier (error, result) {
-      if (options.shouldPass) {
-        verifyPositive(error, result)
-      } else {
-        verifyNegative(error, result)
-      }
-    }
-
-    function verifyPositive (error, result) {
-      if (error) {
-        throw new VError(error, 'Should not throw error')
-      }
-
-      if (result !== options.testInput) {
-        throw new Error(options.method + ' should return input')
-      }
-    }
-
-    function verifyNegative (error, result) {
-      if (!error) {
-        throw new Error(options.method + ' should fail')
-      }
-    }
-
-    var passOrFail = options.shouldPass ? 'pass' : 'fail'
-
-    var description = options.method + ' should ' + passOrFail + ' for ' +
-      stringify(options.testInput)
-
-    test.description = description
-
-    return test
-  }
-
-  module.exports = module.exports.concat(
-    [
-      {
-        method: 'equal',
-        testInput: 'equal',
-        willPass: 'equal',
-        willFail: 'not equal'
-      },
-      {
-        method: 'equal',
-        testInput: {
-          equal: 'equal'
-        },
-        willPass: {
-          equal: 'equal'
-        },
-        willFail: {
-          equal: 'not equal'
-        }
-      },
-      {
-        method: 'match',
-        testInput: /\d/,
-        willPass: 'a number: 3!',
-        willFail: 'no number'
-      },
-      {
-        method: 'type',
-        testInput: 'String',
-        willPass: '3',
-        willFail: 3
-      },
-      {
-        method: 'and',
-        testInput: [
-          globalFunAssert.truthy,
-          globalFunAssert.type('Number')
-        ],
-        willPass: 1,
-        willFail: 0
-      },
-      {
-        method: 'or',
-        testInput: [
-          globalFunAssert.falsey,
-          globalFunAssert.equal(3)
-        ],
-        willPass: 3,
-        willFail: ' '
-      },
-      {
-        method: 'not',
-        testInput: globalFunAssert.match(/\d/),
-        willPass: 'three',
-        willFail: '3'
-      },
-      {
-        method: 'not',
-        testInput: globalFunAssert.or([
-          globalFunAssert.not(globalFunAssert.truthy),
-          globalFunAssert.and([
-            globalFunAssert.equal(4),
-            globalFunAssert.type('Number')
-          ])
-        ]),
-        willPass: ' ',
-        willFail: 4
-      }
-    ].map(testAssertionGenerator)
-  )
-
-  function testAssertionGenerator (options) {
-    var test = funTest({
-      input: options.testInput,
-      verifier: verifier,
-      transformer: transformToMethod(options.method),
-      sync: true
-    })
-
-    function verifier (error, asserter) {
-      if (error) {
-        throw new VError(error, 'should not throw error')
-      }
-
-      verifyPositive(asserter)
-      verifyNegative(asserter)
-    }
-
-    function verifyPositive (asserter) {
-      var originalValue
-      try {
-        originalValue = asserter(options.willPass)
-      } catch (e) {
-        throw new VError(e, options.method + ' should not throw')
-      }
-
-      if (!originalValue === options.testInput) {
-        throw new Error(options.method + ' should return original value')
-      }
-    }
-
-    function verifyNegative (asserter) {
-      var thrownError
-
-      try {
-        asserter(options.willFail)
-      } catch (e) {
-        thrownError = e
-      }
-
-      if (!thrownError) {
-        throw new Error('Should throw error')
-      }
-    }
-
-    var description = options.method + '(' + stringify(options.testInput) +
-      ')' + ' should pass for ' + stringify(options.willPass) +
-      ' and should fail for ' + stringify(options.willFail)
-
-    test.description = description
-
-    return test
-  }
-
-  function transformToMethod (method) {
-    return function transformer (funAssert) {
-      return funAssert[method]
-    }
-  }
-
-  function stringify (anything) {
-    var options = {
-      handlers: {
-        function: stringifier.strategies.toStr()
-      }
-    }
-
-    return stringifier.stringify(anything, options)
+  function matchDigit (funAssert) {
+    return funAssert.match(/\d/)
   }
 })()
 
